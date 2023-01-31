273b9854173220e8fb5a165a02ed0b17025e3287
Lorenzo Patriziano <lorepatri03@gmail.com>
2023-01-31 15:11:04 +0100

    Primo update

--- esercizi/assets/js/scripts.js
+++ esercizi/assets/js/scripts.js
@@ -1,17 +1,27 @@
 'useStrict'
+
 let done = 0;
 function converter() {
-    let input = document.getElementById('input').value;
-    let results = document.getElementById('results');
+    
+    let select = document.getElementById('select1'); //salvo nella variabile select il mio tag select dell'html
+    let value = select.options[select.selectedIndex].value; //salvo nella variabile value, il value dell'opzione scelta nel select
+    if(value != 'all' ){  // con questo if controllo se nella select di html ha scelto un opzione che non sia con value "all", se la condizione è vera, salvo nel mio array la stringa della valuta selezionata
+    value = select.options[select.selectedIndex].value;   
+    arrayValute = [value]    
+    }else{  //se l'opzione scelta ha value 'all' allora salvo nel mio array tutte le valute
+        arrayValute = ["USD","JPY","GBP","CAD","AUD","CHF"]
+    }
+    let input = document.getElementById('input').value; //input messo dall'utente
+    let results = document.getElementById('results');// è un article in cui stamperò successivamente i risultati
     let inputVal = input; //il valore preso dall'input lo salvo in un'altra variabile ancora
-    if (done !=1){    //ho usato questo if per verificare la variabile 'done' in modo che non mi stampa i risultati ogni volta che clicco il bottone
+    if (done !=5){    //ho usato questo if per verificare la variabile 'done' in modo che mi stampi i risultati fino a massimo 5 click
         fetch('https://open.er-api.com/v6/latest/EUR')
             .then(response => response.json()) 
             .then(convertedList => {
-            let arrayValute = ['USD','JPY','GBP','CAD','AUD','CHF'];
+            
             let i = 0;
 
-            for(i ; i < 6; i++ ) {
+            for(i ; i < arrayValute.length; i++ ) {
                 //console.log(convertedList.rates[arrayValute[i]]) //controllo che mi abbia preso le valute giuste
                 let valuta = convertedList.rates[arrayValute[i]]
                 console.log(valuta) //controllo che mi abbia salvato i valori delle valute che volevo io
@@ -21,8 +31,10 @@ function converter() {
                 paragraph.innerText =  + inputVal + " Euro diventano "+  input +" "+ arrayValute[i];
                 results.appendChild(paragraph);
                 input = inputVal //alla fine di ogni ciclo FOR riporto input al suo valore originale (quello che l'utente digita su html nel tag input)
+                console.log(arrayValute);
             }  
         });
+        done ++
     }
-    done = 1
+    
 };
